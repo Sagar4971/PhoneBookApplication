c01a1f4d2a0c02340aedd5c08565625028cac95e
Sagar4971 <sagarnerkar2912@gmail.com>
2022-06-18 23:47:38 +0530

    PHN-21:create controller layer method for delete record

--- src/main/java/com/BikkadIT/PhoneBookApplication/controller/ContactController.java
+++ src/main/java/com/BikkadIT/PhoneBookApplication/controller/ContactController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -67,6 +68,17 @@ public class ContactController {
 		}else {
 			String msg="Contact Not Updated";
 		return new ResponseEntity<String>(msg,HttpStatus.BAD_REQUEST);
+		}
+	}
+	
+	@DeleteMapping("/deleteContactById/{cid}")
+	public String deleteContactById(@PathVariable("cid") Integer cid) {
+		
+		boolean deleteById = contactServiceI.deleteById(cid);
+		if(deleteById == true) {
+			return "Record Deleted Successfully";
+		}else {
+		return "Record Not Deleted";
 		}
 	}
 }
